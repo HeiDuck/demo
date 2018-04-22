49a0a37793221edb9b5fa9edb8d94039ebed9f33
MrLee <1446529778lsh@gamil.com>
2018-04-22 19:35:56 +0800

    fix

--- src/main/java/com/agilecontrol/robotorder/demo/DemoApplication.java
+++ src/main/java/com/agilecontrol/robotorder/demo/DemoApplication.java
@@ -1,7 +1,7 @@
 package com.agilecontrol.robotorder.demo;
 
-import java.util.List;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -11,21 +11,23 @@ import com.agilecontrol.robotorder.demo.utils.DbUtils;
 
 @SpringBootApplication
 public class DemoApplication {
+	public static Logger logger = LoggerFactory.getLogger(DemoApplication.class);
 	public static void main(String[] args) throws Exception {
 		ApplicationContext context = SpringApplication.run(DemoApplication.class, args);
 		System.out.println("Hello World 嘿嘿丫丫");
 		DbUtils.applicationContext = context;
 		RedisConnection connection = DbUtils.getConnection();
 		String key = "usr:893:list";
-		for(int i = 0;i < 100;i++) {
-			List<byte[]> list = connection.bRPop(500, key.getBytes());
-			System.out.println("key:" + new String(list.get(0)));
-			System.out.println("value:" + new String(list.get(1)));
-			Thread.sleep(2000);
-		}
 //		for(int i = 0;i < 100;i++) {
-//			connection.lPush(key.getBytes(), new byte[] {(byte)i});
+//			List<byte[]> list = connection.bRPop(500, key.getBytes());
+//			System.out.println("key:" + new String(list.get(0)));
+//			System.out.println("value:" + new String(list.get(1)));
+//			Thread.sleep(2000);
 //		}
+		for(int i = 0;i < 1000;i++) {
+			logger.debug("key--------------" + i);
+			connection.lPush(key.getBytes(), String.valueOf(i).getBytes());
+		}
 		
 	}
 }
