5684006501c8de55d2525555e9cf0ce80ac5cb7c
MrLee <1446529778lsh@gamil.com>
2018-05-01 21:50:00 +0800

    add sqs

--- src/main/java/com/agilecontrol/robotorder/demo/DemoApplication.java
+++ src/main/java/com/agilecontrol/robotorder/demo/DemoApplication.java
@@ -10,26 +10,28 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.data.redis.connection.RedisConnection;
 
 import com.agilecontrol.robotorder.demo.utils.DbUtils;
+import com.amazonaws.services.sqs.AmazonSQS;
+import com.amazonaws.services.sqs.model.Message;
+import com.amazonaws.services.sqs.model.ReceiveMessageResult;
 
 @SpringBootApplication
 public class DemoApplication {
 	public static Logger logger = LoggerFactory.getLogger(DemoApplication.class);
 	public static void main(String[] args) throws Exception {
 		ApplicationContext context = SpringApplication.run(DemoApplication.class, args);
-		System.out.println("Hello World 嘿嘿丫丫");
+		System.out.println("Hello World");
 		DbUtils.applicationContext = context;
-		RedisConnection connection = DbUtils.getConnection();
-		String key = "usr:893:list";
-		long length = connection.lLen(key.getBytes());
-//		while(length > 0) {
-//			List<byte[]> list = connection.bRPop(500, key.getBytes());
-//			logger.debug("key:" + new String(list.get(0)));
-//			logger.debug("value:" + new String(list.get(1)));
-//			Thread.sleep(2000);
-//		}
-		for(int i = 0;i < 100000;i++) {
-			logger.debug("key--------------" + i);
-			connection.lPush(key.getBytes(), String.valueOf(i).getBytes());
+		AmazonSQS sqs = DbUtils.getSQSClient();
+		String queue_url = sqs.getQueueUrl("sqs_test").getQueueUrl();
+		ReceiveMessageResult rmr = sqs.receiveMessage(queue_url);
+		List<Message> message = rmr.getMessages();
+		if(message == null || message.size() == 0) {
+			logger.debug("没有可用消息");
+		}else {
+			logger.debug("可用消息为:" + message.get(0).getBody());
 		}
+		
+		System.exit(0);
+	    //sqs.deleteMessage(queue_url, message.getMessages().get(0).getReceiptHandle());
 	}
 }
--- src/main/java/com/agilecontrol/robotorder/demo/utils/DbUtils.java
+++ src/main/java/com/agilecontrol/robotorder/demo/utils/DbUtils.java
@@ -3,10 +3,16 @@ package com.agilecontrol.robotorder.demo.utils;
 import org.springframework.context.ApplicationContext;
 import org.springframework.data.redis.connection.RedisConnection;
 
+import com.amazonaws.services.sqs.AmazonSQS;
+
 public class DbUtils{
 	public static ApplicationContext applicationContext;
 
 	public static RedisConnection getConnection() {
 		return applicationContext.getBean(RedisConnection.class);
+	}
+	
+	public static AmazonSQS getSQSClient () {
+		return applicationContext.getBean(AmazonSQS.class);
 	}
 }
